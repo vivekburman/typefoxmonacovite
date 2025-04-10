7669cc7ba61f963b3bc7c751ce1244b90e680c32
Vivek Burman <vivekburman1997@gmail.com>
2025-04-10 22:28:06 +0530

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import importMetaUrlPlugin from '@codingame/esbuild-import-meta-url-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: {
+		port: 4001
+	},
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+        plugins: [
+            importMetaUrlPlugin
+        ]
+    }
+  },
 })
